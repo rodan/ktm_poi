12c3aded2ac77db02c3ffee2bf8c65d6f8975776
Petre Rodan <c59ccef70aa1387612c4@subdimension.ro>
2018-09-01 06:35:10 +0300

    new API url

--- get_dealers.sh
+++ get_dealers.sh
@@ -5,7 +5,7 @@ eu_countries="andorra austria belarus belgium bulgaria croatia czech+republic de
 get_countries() {
     for country in ${eu_countries}; do
         sleep 2
-        curl --silent "http://www.ktm.com/en/api/dealer/search?location=${country}&isCountrySearch=true" 2>&1 | iconv -f utf-8 -t ascii//TRANSLIT | sed 's|<p>||g;s|<br/>||g;s|</p>||g;s|,"[a-z]*":null||g' > "tmp/${country}.json"
+        curl --silent "https://www.ktm.com/en/api/dealer/search?location=${country}&isCountrySearch=true" 2>&1 | iconv -f utf-8 -t ascii//TRANSLIT | sed 's|<p>||g;s|<br/>||g;s|</p>||g;s|,"[a-z]*":null||g' > "tmp/${country}.json"
     done
 }
 
